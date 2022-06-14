57797908f42f655227c957530e106e270cf9ea1b
waccess.krivtsov <O;O$'LMMRt7^GDjH1M,=>
2022-06-14 10:59:10 +0300

    IDLER-GATLING: changed config file

--- src/gatling/scala/com/idler/config/Config.scala
+++ src/gatling/scala/com/idler/config/Config.scala
@@ -8,13 +8,13 @@ object Config {
 
   private[idler] val baseUrl: String = System.getProperty("Url", "http://localhost:8080")
   private[idler] val genericPasswordHash: String = System.getProperty("passwordhash", "3162383261326264b1b3773a05c0ed0176787a4f1574ff0075f7521e")
-  private[idler] val RPS: Double = System.getProperty("Rps", "500").toInt
+  private[idler] val RPS: Double = System.getProperty("Rps", "200").toInt
   private[idler] val LOOP: Int = System.getProperty("Loop", "1").toInt
-  private[idler] val ramp: Int = System.getProperty("Ramp", "1").toInt
-  private[idler] val scnDuration: Int = System.getProperty("Duration", "10").toInt
+  private[idler] val ramp: Int = System.getProperty("Ramp", "10").toInt
+  private[idler] val scnDuration: Int = System.getProperty("Duration", "100").toInt
 
-//  private[idler] val scenarioType: String = System.getProperty("Scenario", "chain")
-  private[idler] val scenarioType: String = System.getProperty("Scenario", "one_request")
+  private[idler] val scenarioType: String = System.getProperty("Scenario", "chain")
+//  private[idler] val scenarioType: String = System.getProperty("Scenario", "one_request")
 
   private[idler] val connectionUrl = "jdbc:postgresql://localhost:5432/idler?currentSchema=idler&user=idler&password=qwerty"
   private[idler] val db = Database.forURL(connectionUrl, driver = "org.postgresql.Driver")
